3f1d207d0ff1c1f74d62396a371a4e483a6fb296
Fouzia-Oreen <fouziaoreen236@gmail.com>
2024-09-26 20:53:18 +0600

    login route created

--- src/app/api/users/login/route.ts
+++ src/app/api/users/login/route.ts
@@ -1,1 +1,41 @@
- 
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { connect } from '@/dfConfig/dbConfig';
+import User from '@/models/userModel';
+import { NextRequest, NextResponse } from 'next/server';
+import bcryptjs from "bcryptjs";
+import jwt from 'jsonwebtoken'
+connect()
+
+export async function POST(request:NextRequest) {
+    try {
+        const reqBody = await request.json()
+        const {email, password}  = reqBody
+        console.log(reqBody);
+
+        const user = await User.findOne({email})
+        if (!user) {
+            return NextResponse.json({error: "User doesnot exist"}, {status: 400})
+        }
+        const validPassword = await bcryptjs.compare(password, user.password)
+        if (!validPassword) {
+            return NextResponse.json({error: "Check your credentials"}, {status: 400})
+        }
+        /// jwt token
+        const tokenData = {
+            id:user._id,
+            firstname:user.firstname,
+            email:user.email
+        }
+        const jwtToken = await jwt.sign({tokenData},process.env.TOKEN_SECRET!, {expiresIn : "1d"})
+        const response = NextResponse.json({
+            message:"Logged in success",
+            success: true
+        })
+        const cookies = response.cookies.set("token", jwtToken, {
+            httpOnly:true
+        })
+        return cookies
+    } catch (error: any) {
+        return NextResponse.json({error:error.message},{ status: 500})
+    }
+}
