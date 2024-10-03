9272de213e9541e4e90c4a7c629307b21fe2d237
Fouzia-Oreen <fouziaoreen236@gmail.com>
2024-10-03 21:34:54 +0600

    middleware added

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -22,8 +22,8 @@ export default function ProfilePage() {
     }
 
     const getUserDetails = async () => {
-        const res = await axios.get('/api/users/me')
-        console.log(res.data);
+        const res = await axios.get('/api/users/profile')
+        console.log(res.data.data._id);
         setData(res.data.data._id)
     }
 
@@ -34,11 +34,12 @@ export default function ProfilePage() {
             <p>Profile page</p>
             <h2 className="p-1 rounded bg-green-500">{data === 'nothing' ? "Nothing" : <Link href={`/profile/${data}`}>{data}
             </Link>}</h2>
-        <hr />
-        <button
-        onClick={logout}
-        className="bg-blue-500 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        >Logout</button>
+            <hr />
+            <button
+            onClick={logout}
+            className="bg-blue-500 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            >Logout
+            </button>
 
         <button
         onClick={getUserDetails}
